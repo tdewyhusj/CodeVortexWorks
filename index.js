1bd626a6fd5452e5f9e48e6cfa857fce6fcce1e8
tdewyhusj <gs1bilaljal@hotmail.com>
2024-05-08 03:20:52 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const sum = (a, b) => a + b;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
